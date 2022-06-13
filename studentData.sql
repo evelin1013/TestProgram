0dd5e5739bdae026d847d05fb868c545433d683e
Sriram <sriram.srinivasan@vastpro.in>
2022-06-13 15:22:11 +0530

    Sql table

---
+++ studentData.sql
@@ -0,0 +1,9 @@
+create table studentDate
+(
+	id smallInt not null,
+	roll_Num smallint primary key,
+	name varchar(30) not null,
+	age smallint ,
+	totalMarks smallint not null,
+	grade char(1) not null
+);
